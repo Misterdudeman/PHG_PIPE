70aac326d16f0dae28468870669f3c27f01b465e
Misterdudeman <misterdudeman@gmail.com>
2018-11-08 02:34:36 -0800

    Added Logging, file checks and warnings and image sorting functions.

--- merge_hdr_sets.py
+++ merge_hdr_sets.py
@@ -25,13 +25,22 @@ class merg_hdr_sets(object):
 
     """
 
-    def __init__(self, steps=None, recursive=None, high=None, low=None, cwd=None):
+    def __init__(self, steps=None, recursive=None,
+                 verbose=None, high=None, low=None, cwd=None):
         """Initialize class arguments."""
         self.steps = steps
         self.recursive = recursive
+        self.verbose = verbose
         self.high = high
         self.low = low
         self.cwd = cwd
+
+    def logging(self, verbose):
+        """Initialize logging."""
+        logging.basicConfig(level=logging.INFO)
+        logger = logging.getLogger(__name__)
+        logger.propagate = verbose
+        logger.info('Starting logs')
 
     def parse_args(self, argv=None):
         """Parse Args from class and argparse."""
@@ -43,6 +52,9 @@ class merg_hdr_sets(object):
         parser.add_argument('--r',
                             help="set recursive folder search",
                             action="store_true")
+        parser.add_argument('--v',
+                            action="store_true",
+                            help="set verbose")
         parser.add_argument('--hi',
                             type=str,
                             help="high res image format",
@@ -53,10 +65,13 @@ class merg_hdr_sets(object):
                             default="jpg")
 
         args, not_used = parser.parse_known_args(argv)
+        self.args = args
         self.steps = args.s
         self.recursive = args.r
+        self.verbose = args.v
         self.high = args.hi
         self.low = args.lo
+        self.cwd = os.getcwd()
 
     def sort_by_type(self, files, file_list, type_list):
         """Sorts files into type lists."""
@@ -65,18 +80,21 @@ class merg_hdr_sets(object):
         for file in files:
                 kind = filetype.guess_extension(file)
                 if kind == hr_type:
+                    logging.info("Image is High Res: " + file)
                     hr_list.append(file)
                     pass
                 elif kind == lr_type:
+                    logging.info("Image is Low Res: " + file)
                     lr_list.append(file)
                     pass
                 else:
+                    logging.info("File is Other: " + file)
                     bin.append(file)
                     pass
         return file_list
 
     def get_image_types(self, arguments, cwd):
-        """"""
+        """Get images by type."""
         if arguments:
             highres_list, lowres_list, bin = ([] for i in range(3))
             file_list = [highres_list, lowres_list, bin]
@@ -87,10 +105,48 @@ class merg_hdr_sets(object):
 
             if arguments.r:
                 for root, dirs, files in os.walk(cwd):
-                    file_list = self.sort_by_type(files, file_list, type_list)
+                    logging.info("Script is collecting images recursively")
+                    out_lists = self.sort_by_type(files, file_list, type_list)
 
             else:
-                for files in os.listdir(cwd):
-                    file_list = self.sort_by_type(files, file_list, type_list)
+                logging.info('Script is collecting images locally')
+                files = os.listdir(cwd)
+                out_lists = self.sort_by_type(files, file_list, type_list)
+        return out_lists
+
+    def check_lists(self, sorted_lists):
+        """Check image lists and throw warnings."""
+        hr_list, lr_list, bin_list = sorted_lists
+        hr_len = len(hr_list)
+        lr_len = len(lr_list)
+        if hr_len != lr_len:
+            logging.warning("High Res Images do not match proxy.")
+        elif len(bin_list) > max(len(hr)):
+            logging.warning("Too many unrecognized files.")
+
+    def make_set(self, high, low):
+        """Make image set."""
+        self.high = high
+        self.low = low
+
+    def sort_image_sets(self, sorted_lists, steps):
+        """Sort images into sets."""
+        counter = 0
+        set_count = 0
+        high_set_list = []
+        low_set_list = []
+        for high, low in enumerate(sorted_lists):
+            print(counter)
+            if counter >= steps:
+                set_count += 1
+                logging.info("Set: " + set_count)
+                counter = 0
+            else:
+                counter += 1
+
 if __name__ == "__main__":
-    mhs = merg_hdr_sets.parse_args(['--s 3', '--r'])
+    mhs = merg_hdr_sets()
+    mhs.parse_args(['--s 3', '--r'])
+    mhs.logging(mhs.verbose)
+    images = mhs.get_image_types(mhs.args, mhs.cwd)
+    mhs.sort_image_sets(images, mhs.steps)
